629528cbb90935cd9f6b7fa8ecb82277f0d549f4
Anzx <0.love.2d.atsi.2@gmail.com>
2020-10-25 16:50:48 +0200

    ♥

---
+++ Assets/Scripts/Game/Health.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System;
+
+public class Health
+{
+    public int maxHP 
+    {
+        get 
+        {
+            return maxHP;
+        }
+
+        private set 
+        {
+            if (!alive) 
+            {
+                throw new Exception("");
+            }
+
+            maxHP = value;
+        }
+    }
+
+    private int _hp = 100;
+
+    public bool alive {
+        get 
+        {
+            if (_hp <= 0)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+
+    public void AddHP(int hp)
+    {
+        _hp += hp;
+    }
+
+    public void AddDamage(int damage)
+    {
+        _hp -= damage;
+    }
+}

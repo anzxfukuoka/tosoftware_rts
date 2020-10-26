a320e113ac21d7e8941c79c0f6087d8d0cf007b9
Gleb89 <GlebLobach@vira.games>
2020-10-26 10:35:15 +0200

    add character class

---
+++ Assets/Scripts/Game/Robot/Character.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character
+{
+    private Experience _experience;
+    private Health _health;
+    private Energy _energy;
+
+    Character(int maxHealth = 100, int maxEnergy = 100)
+    {
+        _experience = new Experience();
+        _health = new Health(maxHealth);
+        _energy = new Energy(maxEnergy);
+    }
+
+    ~Character()
+    {
+        _experience = null;
+        _health = null;
+        _energy = null;
+    }
+
+
+}

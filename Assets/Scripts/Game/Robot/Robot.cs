f8c2ec63ed0dcde5ce521f2e73a487441870a6a3
Gleb89 <GlebLobach@vira.games>
2020-10-26 15:08:34 +0200

    add robot class

---
+++ Assets/Scripts/Game/Robot/Robot.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Robot : PC
+{
+    public Robot(int maxHP = 100, int maxEnergy = 100)
+    {
+        health = new Health(maxHP);
+        energy = new Energy(maxEnergy);
+        experience = new RobotExperience();
+    }
+}

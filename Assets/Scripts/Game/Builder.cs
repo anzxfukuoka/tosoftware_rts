7efb019c9398a80fc1688a7400daabc09ca12749
Anzx <0.love.2d.atsi.2@gmail.com>
2020-10-27 16:31:34 +0200

    building class

---
+++ Assets/Scripts/Game/Builder.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Builder
+{
+    private Energy _energySource;
+
+    public Builder(Energy energySource) {
+        this._energySource = energySource;
+    }
+
+    public void Build(Building building) {
+
+        building.Build(_energySource);
+    }
+}
---
+++ Assets/Scripts/Game/NotificationSystem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NotificationSystem : MonoBehaviour
+{
+    public delegate void Messege(string msg);
+
+    public static event Messege DebugMSG;
+    public static event Messege UserMSG;
+
+    void Start()
+    {
+        DebugMSG += Debug.Log; //вывод в консоль юнити
+        UserMSG += Debug.Log;
+    }
+
+    public static void DebugMsg(string msg) 
+    {
+        DebugMSG.Invoke("DEBUG: " + msg);
+    }
+
+    public static void UserMsg(string msg)
+    {
+        UserMSG.Invoke("GAME: " + msg);
+    }
+}

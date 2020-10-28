62f3f88f8f123fa998fe8acfb73e1a7f6b516216
nihhilist <71274110+nihhilist@users.noreply.github.com>
2020-10-28 08:37:37 +0200

    Add files via upload

---
+++ modul.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Module
+{
+    public GameTransform transform;
+    private static List<Building> buildings;
+    public Module(GameTransfrom t, Buildings[] alowableBuildings)
+    {
+        foreach (Buildings b in alowableBuildings)
+        {
+            buildings.Add(b);
+        }
+        transform = t;
+    }
+
+}

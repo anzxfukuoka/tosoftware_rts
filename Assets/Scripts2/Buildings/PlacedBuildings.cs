ba0daffe807d7434cf15fa21ee01df6629c22e66
Gleb89 <GlebLobach@vira.games>
2020-11-16 07:24:38 +0200

    Make PlacedBuildings ScriptableObject

--- Assets/Scripts2/Buildings/PlacedBuildings.cs
+++ Assets/Scripts2/Buildings/PlacedBuildings.cs
@@ -3,9 +3,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+[CreateAssetMenu(menuName = "Buildings/PlacedBuildings")]
 public class PlacedBuildings : ScriptableObject
 {
-    public Dictionary<string, PlacedBuilding> buildingsTypes;
+    [System.Serializable]
+    public class PlacedBuildingPair : SerializableDictinary<string, PlacedBuilding>.DictinaryPage
+    {
+        
+    }
+
+    [System.Serializable]
+
+    public class Dictinary
+    {
+        [SerializeField] public List<PlacedBuildingPair> _dictinary = new List<PlacedBuildingPair>();
+        public PlacedBuilding this[string key]
+        {
+            get => this._dictinary.Find((x) => x.Key == key).Value;
+        }
+    }
+
+
+    [SerializeField] public Dictinary buildingsTypes;
 
     public PlacedBuilding FindBuildingByName(string name) 
     {
---
+++ Assets/Scripts2/Helpers/SerializableDictinary.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class SerializableDictinary<K,V>
+{
+    [SerializeField] public List<DictinaryPage> _dictinary = new List<DictinaryPage>();
+    [System.Serializable]
+    public class DictinaryPage
+    {
+        public K Key;
+        public V Value;
+    }
+
+
+    public V this[K key]
+    {
+        get => this._dictinary.Find((x) => EqualityComparer<K>.Default.Equals(x.Key, key)).Value;
+    }
+
+
+
+}
